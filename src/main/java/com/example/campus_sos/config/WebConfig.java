3b3289cfeafa49ab0a74644295ac551c0984667c
김재웅 <grbuguj@users.noreply.github.com>
2025-07-08 12:57:15 +0900

    feat: 프론트 코스 추가

--- src/main/java/com/example/campus_sos/config/WebConfig.java
+++ src/main/java/com/example/campus_sos/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000",
-                                "https://campus-sos.duckdns.org"
+                                "https://campus-sos.duckdns.org",
+                                "https://campus-sos.vercel.app"
                                 ) // 프론트 로컬 주소. 배포시에는 배포 주소로 변경해야 함
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
